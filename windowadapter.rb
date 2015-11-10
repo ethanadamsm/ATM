3365c4298a8c57ac6c1a8995916ac8a482caed9f
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-11-10 09:50:30 -0700

    Add new buttons

--- windowadapter.rb
+++ windowadapter.rb
@@ -9,6 +9,8 @@ class	WindowAdapter < Adapter
 		@background = Gosu::Image.new("display/background.png")
 		@buttons = []
 		@buttons.push(Button.new(20, 20, 100, 100, "Withdraw"))
+		@buttons.push(Button.new(140, 20, 100, 100, "Deposit"))
+		@buttons.push(Button.new(260, 20, 100, 100, "Balance"))
 		@window = Window.new(@background, @buttons)
 		@window.show
 	end
