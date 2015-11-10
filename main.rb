c12746acdbec3054caef6f990608aa6c640fdde9
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-11-10 09:53:43 -0700

    Add comments for user

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require_relative "account"
 require_relative "consoleadapter"
 require_relative "windowadapter"
 
-#console = ConsoleAdapter.new
+#commenting out the line below will result in a window for atm
+#console = ConsoleAdapter.new 
+#commenting out the line below will result in a command line atm
 window = WindowAdapter.new
 
 
